909fbd9586aedf5b5d65385e7a4fceeb5d03a4c6
kalse pooja <kalsepooja056@gmail.com>
2024-02-15 23:02:09 +0530

    work on template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild("signUpForm") signUpForm !: NgForm
   title = 'TDForms';
+  userInfo = {
+    "userDetails": {
+      "userName": 'nathan',
+      "email": "Nt@gmail.co."
+    },
+    "secreteQuestion": "favorateBook",
+    "answer": "woman",
+    "issubscribed": true,
+    "contactMode": "Email"
+  }
+  contactMode = [
+    {moc: "email", id: 1},
+    {moc: "phone", id: 2}
+  ]
+
+  // onSignUp(signUp: NgForm){
+  //   if(signUp.valid){
+  //     console.log(signUp)
+  //     console.log(signUp.value);
+  //   }
+  // }
+  content(eve: NgModel){
+    if(eve.hasError('required')){
+      return `this feild is required`
+    }else if(eve.hasError('pattern')){
+      return `incorrect pattern`
+    } else if(eve.hasError('minlength')){
+      return ` minimum length should be 6`
+    }else if(eve.hasError('maxlength')){
+      return ` maximum length should be 10`
+    }else{
+      return
+    }
+  }
+
+
+  onSignUp(signForm: NgForm){
+    if(this.signUpForm.valid){
+      console.log(this.signUpForm.value)
+    }
+  }
+
+  patchValue(){
+      this.signUpForm.form.patchValue(this.userInfo)
+  }
 }
